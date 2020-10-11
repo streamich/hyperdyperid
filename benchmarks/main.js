cda3d98a07627d5b3cb2c07d0dde02517d4c34eb
streamich <streamich@gmail.com>
2020-10-12 01:40:05 +0200

    feat: 🎸 implement fast ID generators

--- benchmarks/main.js
+++ benchmarks/main.js
@@ -1,16 +1,33 @@
 const Benchmark = require('benchmark');
 const hyperid = require('hyperid');
+const {makexorshift32} = require('../lib/xorshift32');
+const {str5_36} = require('../lib/str5_36');
+const {str6_36} = require('../lib/str6_36');
+const {str10_36} = require('../lib/str10_36');
 
 const generateHyperId = hyperid();
+const xorshift32 = makexorshift32();
 
 const suite = new Benchmark.Suite;
 
 suite
-  .add('Math.random()', function() {
-    Math.random();
+  .add('xorshift32()', function() {
+    xorshift32();
+  })
+  .add('str5_36()', function() {
+    str5_36();
+  })
+  .add('str6_36()', function() {
+    str6_36();
+  })
+  .add('str10_36()', function() {
+    str10_36();
   })
   .add('hyperid', function() {
     generateHyperId();
+  })
+  .add('Math.random().toString()', function() {
+    Math.random().toString();
   })
 
 suite
